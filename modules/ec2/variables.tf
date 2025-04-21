4fba9e233ba165267ba3a719cac34e1e8fa65a2e
JatinThakur44 <128354764+JatinThakur44@users.noreply.github.com>
2025-04-21 11:20:59 +0530

    Add files via upload

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,28 @@
+
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID for the instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the instance"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+  default = "web-instance"
+}
+
+
+
