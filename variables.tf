c64da2ae993209ff37cbe76f60ed87fabd56891c
JatinThakur44 <128354764+JatinThakur44@users.noreply.github.com>
2025-04-22 13:10:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,6 +27,17 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
 
 
 # variable "ami" {
